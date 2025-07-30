25d84898c39886c8cd7b3a1d27480131e55ad446
antosquicciarini <a.squicciarini@alumnos.upm.es>
2025-07-30 12:22:31 +0200

    Initial commit: structured ML project using cookiecutter-data-science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Multiclass classification of forest cover type using the Forest Cover Type dataset (UCI). Includes EDA, preprocessing, model evaluation, and selection based on performance metrics. Fully structured and reproducible project.',
+    author='Antonio Squicciarini',
+    license='MIT',
+)
